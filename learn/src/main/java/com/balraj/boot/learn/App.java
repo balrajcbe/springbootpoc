73a8771159ce2ca7bd0b176e17ad38a5e7e72f7a
balrajcbe <balraj1610@gmail.com>
2017-08-12 15:24:40 +0530

    Basic sprint boot application

--- learn/src/main/java/com/balraj/boot/learn/App.java
+++ learn/src/main/java/com/balraj/boot/learn/App.java
@@ -1,13 +1,18 @@
 package com.balraj.boot.learn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Balraj" );
+        SpringApplication.run(App.class, args);
     }
 }
