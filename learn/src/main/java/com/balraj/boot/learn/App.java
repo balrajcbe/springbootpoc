80c1ce7f5d2df69148e6b8eed342d8091464c7d2
balrajcbe <balraj1610@gmail.com>
2017-08-12 14:22:04 +0530

    First commit

---
+++ learn/src/main/java/com/balraj/boot/learn/App.java
@@ -0,0 +1,13 @@
+package com.balraj.boot.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
