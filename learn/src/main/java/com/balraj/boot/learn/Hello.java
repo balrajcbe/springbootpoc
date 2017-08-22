bdfd06482e206346eeffab3b6e8d95f8f3a669da
balrajcbe <balraj1610@gmail.com>
2017-08-22 19:23:54 +0530

    Get method implementation

--- learn/src/main/java/com/balraj/boot/learn/Hello.java
+++ learn/src/main/java/com/balraj/boot/learn/Hello.java
@@ -1,6 +1,8 @@
 package com.balraj.boot.learn;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,12 @@ public class Hello {
 	@RequestMapping("hello")
 	public String home() {
 		return "Hello world";
+	}
+	
+	@RequestMapping(value = "batman/{name}", method = RequestMethod.GET)
+	public String batman(@PathVariable String name) {
+		System.out.println("Received input: "+ name);
+		return name;
 	}
 
 }
